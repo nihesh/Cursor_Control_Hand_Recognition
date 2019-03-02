7477bf68cd1dbc28fb67a3401d052312b9e8fe06
HarshPathakhp <harsh16041@iiitd.ac.in>
2019-03-02 16:11:04 +0530

    Annotations.csv

--- EgohandsPreprocessor.py
+++ EgohandsPreprocessor.py
@@ -14,16 +14,38 @@ Generates Egohands folder in the cwd
 
 import sys
 from scipy.io import loadmat
-
+import pandas as pd
+import numpy as np
+import os
 # Absolute/Relative path to Egohands dataset
 ROOT = None
 
-if(__name__ == "__main__"):
 
-	ROOT = "./../Dataset/egohands_data"
+ROOT = "./../Dataset/egohands_data"
 
-	annotations = loadmat(ROOT+"/metadata.mat")
-	print(annotations["video"][0][0])
+annotations = loadmat(ROOT+"/metadata.mat")
+x = annotations['video']
 
-
+df = pd.DataFrame(columns = ['Filename', 'LabelId', 'Label', 'xmin', 'ymin', 'xmax', 'ymax'])
+counter = 0
+l = []
+for fid in range(0, x.shape[1]):
+	folderarr = x[0, fid][6]
+	for imgid in range(0, folderarr.shape[1]):
+		temp = folderarr[0, imgid]
+		lenth = len((folderarr[0, imgid]))
+		for iter in range(1, lenth):
+			imgname = str(temp[0][0][0])
+			for ad in range(0, 4 - len(imgname)):
+				imgname = '0' + imgname
+			l.append(imgname)
+			if(temp[iter].shape[0] != 0):
+				n = temp[iter]
+				minxy = (n.min(axis = 0))
+				maxxy = (n.max(axis = 0))
+				df.loc[counter] = [str(imgname) + ".jpg", 1, "Hand", int(minxy[0]), int(minxy[1]), int(maxxy[0]), int(maxxy[1])]
+				counter += 1
+				print(counter)
+print(counter)
+df.to_csv("Annotations.csv", index = False)				
 
