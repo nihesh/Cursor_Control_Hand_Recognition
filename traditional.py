5d4af9505123507c8ae58beb73887cb4110d7ad9
nihesh <nihesh16059@iiitd.ac.in>
2019-04-02 19:06:35 +0530

    Traditional.py - moving averages for stability

--- traditional.py
+++ traditional.py
@@ -9,6 +9,9 @@ import pyautogui
 
 SCALE_FACTOR = 10
 BLIND_SPOT_FRACTION = [1.2, 1.5]
+MOVING_AVERAGE_LENGTH = 5
+LAST_HAND = [np.asarray([0,0])]*MOVING_AVERAGE_LENGTH
+
 
 def Segment(img):
 
@@ -23,16 +26,27 @@ def Segment(img):
 			else:
 				output[i][j] = 0
 
-	return output
+	return np.float32(output) 
 
 def DetectHand(difference):
+
+	global LAST_HAND 
+
+	difference = np.pad(difference,(1,1),"edge")
+	difference = cv2.medianBlur(difference, 5)
 
 	cluster = []
 	for i in range(difference.shape[0]):
 		for j in range(difference.shape[1]):
 			if(difference[i][j]):
 				cluster.append([i,j])
-	return np.mean(cluster, 0)
+	if(len(cluster) != 0):					# Just a hack to prevent spikes in cursor movement
+		LAST_HAND.append(np.mean(cluster, 0))
+		LAST_HAND.pop(0)
+	else:
+		LAST_HAND.append(LAST_HAND[-1])
+		LAST_HAND.pop(0)
+	return np.mean(LAST_HAND,0) 
 
 def Normalise(hand, dim):
 
@@ -41,9 +55,8 @@ def Normalise(hand, dim):
 	center = dim//2
 	mean_shifted = (hand - center)*(BLIND_SPOT_FRACTION)
 	out = (center+mean_shifted).astype(int)
-	out[0] = min(out[0], dim[0]-1)
-	out[1] = min(out[1], dim[1]-1)
-	print(out, dim)
+	out[0] = max(1,min(out[0], dim[0]-1))
+	out[1] = max(1,min(out[1], dim[1]-1))
 
 	return out
 
@@ -51,10 +64,9 @@ def MarkHand(img, coords):
 	out = np.copy(img)
 	for i in range(-3,4):
 		for j in range(-3,4):
-			try:
+			if(coords[0]+i >= 0 and coords[0]+i < img.shape[0] and coords[1]+j>=0 and coords[1]+j<img.shape[1]):
 				out[coords[0]+i][coords[1]+j][2] = 255
-			except:
-				pass
+			
 	return out 
 
 def MoveCursor(hand, dim):
@@ -66,11 +78,11 @@ def MoveCursor(hand, dim):
 	screenWidth, screenHeight = pyautogui.size()
 	out = np.asarray([0,0])
 
-	out[0] = 1+hand[0]*(int(screenWidth/dim[0]))
+	out[0] = 1+hand[0]*(int(screenHeight/dim[0]))
 	out[1] = 1+hand[1]*(int(screenWidth/dim[1]))
-	
+
 	print(out)
-	pyautogui.moveTo(out[0], out[1])
+	pyautogui.moveTo(screenWidth - out[1] + 1, out[0])
 
 if(__name__ == "__main__"):
 
